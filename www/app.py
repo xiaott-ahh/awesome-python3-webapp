8adca9c6f6cfb6c8517e9f62680d5c14264dc635
xiaott <13068729891@163.com>
2019-12-15 17:10:33 +0800

    new version of aiohttp

--- www/app.py
+++ www/app.py
@@ -15,9 +15,18 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>',
-		headers={'content-type':'text/html'})
+	return web.Response(text='awesome python')
 
+def hello(request):
+	name = request.match_info.get('name')
+	text = 'hello' + name
+	return web.Response(text=text)
+
+app = web.Application()
+app.add_routes([web.get('/',index),
+	            web.get('/{name}',hello)])
+web.run_app(app,host='127.0.0.1',port=9000)
+'''
 async def init(loop):
 	app = web.Application(loop=loop)
 	app.router.add_route('GET','/',index)
@@ -28,3 +37,4 @@ async def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+'''
