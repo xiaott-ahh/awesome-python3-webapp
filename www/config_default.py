8c08c19724762a6e8fa0506744b4de4ba4f421a5
xiaott <13068729891@163.com>
2020-07-21 17:01:44 +0800

    add config file

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'QiuJun tao'
+
+configs = {
+    'debug':True,
+    'database':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'www',
+        'password':'www',
+        'database':'awesome'
+    },
+    'session':{
+        'secret':'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'QiuJun tao'
+
+configs = {
+    'database':{
+        'host':'127.0.0.1'
+    }
+}
